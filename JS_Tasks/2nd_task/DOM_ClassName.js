db4654b048fa845c6cc25fab98f359b0ac827ca7
Prem Radheshyam Mahajan <premradheshyammahajan@gmail.com>
2026-09-14 01:34:02 +0530

    not want this all files is CSS branch only

--- JS_Tasks/2nd_task/DOM_ClassName.js
+++
@@ -1,7 +0,0 @@
-// Write your code below:
-const list = document.getElementsByClassName('fruit');
-list[2].style.backgroundColor = 'yellow';
-
-for (let val of list) {
-    val.style.fontWeight = 'bold';
-}
